324b4fe4ca8ea60e60a3f2978d73ed72299441aa
Arthur <125185500+ArthurStep@users.noreply.github.com>
2025-05-08 17:45:24 +0400

    Add files via upload

---
+++ src/main/java/main/artfix/Menu.java
@@ -0,0 +1,21 @@
+package main.artfix;
+
+public class Menu {
+    public static void helpMenu() {
+        System.out.println(" ");
+        System.out.println("*** HELP MENU ***");
+        System.out.println("Help = Help menu.");
+        System.out.println(" ");
+        System.out.println("Date = Date.");
+        System.out.println("Time = Time.");
+        System.out.println("Clock = Clock.");
+        System.out.println("Alarm = Alarm.");
+        System.out.println("Game = Game.");
+        System.out.println("Calc = Calculator.");
+        System.out.println("Set name = Set new Username For User.");
+        System.out.println("Random num = Random number.");
+        System.out.println("Clear = Clear console.");
+        System.out.println("Search = Search information.");
+        System.out.println("Exit or Bye = Stop program.");
+    }
+}
